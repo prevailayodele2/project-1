a248b0e6c1b5ca47435e4cff94566837a3141305
PrevailAyodele <ayodeleprevail@gmail.com>
2022-08-03 02:48:58 +0100

    init

--- src/utilis.js
+++ src/utilis.js
@@ -2,7 +2,7 @@ import ProductStar from "./components/Product/ProductStar"
 
 export const product = [
     {
-        id:1,
+        id:8,
         offpercentage: '15% off',
         image: '/image/img-1 (1).jfif',
         slug:'product1',
